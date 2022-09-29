f538db1cb07859433c4a1e857d2f566bfa49e03c
f0ti <foti.kerkeshi@hotmail.com>
2022-09-29 23:21:57 +0200

    64 bitboard ez, returns single moves

--- isg/impasse/decimal_to_board.py
+++ isg/impasse/decimal_to_board.py
@@ -4,8 +4,9 @@ import numpy as np
 import sys
 
 n = int(sys.argv[1])
-binary = '{:032b}'.format(n)
-board = np.array(list(binary), dtype=int).reshape(-1, 4)
+binary = '{:064b}'.format(n)
+board = np.array(list(binary), dtype=int).reshape(-1, 8)
 
+board = np.flip(board, 1)
 print(binary)
 print(board)
--- isg/impasse/test.py
+++ isg/impasse/test.py
@@ -17,45 +17,35 @@ RANK_NAMES = ["1", "2", "3", "4", "5", "6", "7", "8"]
 Square = int
 
 SQUARES = [
-  A1, C1, E1, G1,
-  B2, D2, F2, H2,
-  A3, C3, E3, G3,
-  B4, D4, F4, H4,
-  A5, C5, E5, G5,
-  B6, D6, F6, H6,
-  A7, C7, E7, G7,
-  B8, D8, F8, H8,
-] = range(32)
+  A1, B1, C1, D1, E1, F1, G1, H1,
+  A2, B2, C2, D2, E2, F2, G2, H2,
+  A3, B3, C3, D3, E3, F3, G3, H3,
+  A4, B4, C4, D4, E4, F4, G4, H4,
+  A5, B5, C5, D5, E5, F5, G5, H5,
+  A6, B6, C6, D6, E6, F6, G6, H6,
+  A7, B7, C7, D7, E7, F7, G7, H7,
+  A8, B8, C8, D8, E8, F8, G8, H8,
+] = range(64)
 
-SQUARE_NAMES = [
-  'A1', 'C1', 'E1', 'G1',
-  'B2', 'D2', 'F2', 'H2',
-  'A3', 'C3', 'E3', 'G3',
-  'B4', 'D4', 'F4', 'H4',
-  'A5', 'C5', 'E5', 'G5',
-  'B6', 'D6', 'F6', 'H6',
-  'A7', 'C7', 'E7', 'G7',
-  'B8', 'D8', 'F8', 'H8',
-]
+Bitboard = int
+BB_EMPTY = 0
+BB_ALL = 0xffff_ffff_ffff_ffff
 
 BB_SQUARES = [
-  BB_A1, BB_C1, BB_E1, BB_G1,
-  BB_B2, BB_D2, BB_F2, BB_H2,
-  BB_A3, BB_C3, BB_E3, BB_G3,
-  BB_B4, BB_D4, BB_F4, BB_H4,
-  BB_A5, BB_C5, BB_E5, BB_G5,
-  BB_B6, BB_D6, BB_F6, BB_H6,
-  BB_A7, BB_C7, BB_E7, BB_G7,
-  BB_B8, BB_D8, BB_F8, BB_H8,
+  BB_A1, BB_B1, BB_C1, BB_D1, BB_E1, BB_F1, BB_G1, BB_H1,
+  BB_A2, BB_B2, BB_C2, BB_D2, BB_E2, BB_F2, BB_G2, BB_H2,
+  BB_A3, BB_B3, BB_C3, BB_D3, BB_E3, BB_F3, BB_G3, BB_H3,
+  BB_A4, BB_B4, BB_C4, BB_D4, BB_E4, BB_F4, BB_G4, BB_H4,
+  BB_A5, BB_B5, BB_C5, BB_D5, BB_E5, BB_F5, BB_G5, BB_H5,
+  BB_A6, BB_B6, BB_C6, BB_D6, BB_E6, BB_F6, BB_G6, BB_H6,
+  BB_A7, BB_B7, BB_C7, BB_D7, BB_E7, BB_F7, BB_G7, BB_H7,
+  BB_A8, BB_B8, BB_C8, BB_D8, BB_E8, BB_F8, BB_G8, BB_H8,
 ] = [1 << sq for sq in SQUARES]
 
-def parse_square(name: str) -> Square:
-  """
-  Gets the square index for the given square *name*
-  (e.g., ``a1`` returns ``0``).
+SQUARE_NAMES = [f + r for r in RANK_NAMES for f in FILE_NAMES]
 
-  :raises: :exc:`ValueError` if the square name is invalid.
-  """
+def parse_square(name: str) -> Square:
+  """Gets the square index for the given square *name*"""
   return SQUARE_NAMES.index(name)
 
 def square_name(square: Square) -> str:
@@ -76,24 +66,13 @@ def square_rank(square: Square) -> int:
 
 def square_mirror(square: Square) -> Square:
   """Mirrors the square vertically."""
-  return square ^ 0x1c
+  return square ^ 0x38
 
 SQUARES_180 = [square_mirror(sq) for sq in SQUARES]
 
 Bitboard = int
 BB_CLEAN = 0
 BB_ALL = 0xffff_ffff_ffff_ffff
-
-BB_RANKS = [
-  BB_RANK_1,
-  BB_RANK_2,
-  BB_RANK_3,
-  BB_RANK_4,
-  BB_RANK_5,
-  BB_RANK_6,
-  BB_RANK_7,
-  BB_RANK_8,
-] = [0xf << (4 * i) for i in range(8)]
 
 BB_FILES = [
   BB_FILE_A,
@@ -104,7 +83,18 @@ BB_FILES = [
   BB_FILE_F,
   BB_FILE_G,
   BB_FILE_H,
-] = [0x0101_0101 << i for i in range(8)]
+] = [0x0101_0101_0101_0101 << i for i in range(8)]
+
+BB_RANKS = [
+  BB_RANK_1,
+  BB_RANK_2,
+  BB_RANK_3,
+  BB_RANK_4,
+  BB_RANK_5,
+  BB_RANK_6,
+  BB_RANK_7,
+  BB_RANK_8,
+] = [0xff << (8 * i) for i in range(8)]
 
 BB_BACKRANKS = BB_RANK_1 | BB_RANK_8
 
@@ -114,57 +104,66 @@ BB_WHITE = BB_B8 | BB_F8 | BB_C7 | BB_G7 | BB_D2 | BB_H2 | BB_A1 | BB_E1
 BB_SINGLES = BB_D8 | BB_H8 | BB_A7 | BB_E7 | BB_D2 | BB_H2 | BB_A1 | BB_E1
 BB_DOUBLES = BB_B8 | BB_F8 | BB_C7 | BB_G7 | BB_B2 | BB_F2 | BB_C1 | BB_G1
 
-BB_EMPTY = np.uint32(~(BB_WHITE | BB_BLACK))
-BB_OCCUPIED = BB_RANK_1 | BB_RANK_2 | BB_RANK_7 | BB_RANK_8
+BB_EMPTY = ~(BB_WHITE | BB_BLACK)
 
 BB_OCCUPIED_CO = [BB_EMPTY, BB_EMPTY]
 BB_OCCUPIED_CO[WHITE] = BB_WHITE
 BB_OCCUPIED_CO[BLACK] = BB_BLACK
 
 # changes
-# BB_OCCUPIED_CO[WHITE] |= BB_B6
-# BB_SINGLES |= BB_B6
+BB_OCCUPIED_CO[WHITE] |= BB_A5
+BB_SINGLES |= BB_A5
+
+BB_OCCUPIED_CO[WHITE] |= BB_G5
+BB_SINGLES |= BB_G5
 
 BB_OCCUPIED = BB_OCCUPIED_CO[WHITE] | BB_OCCUPIED_CO[BLACK]
+print(BB_OCCUPIED)
 
-print(BB_OCCUPIED_CO[BLACK])
-print(BB_EMPTY)
-
-def get_moves(square: int) -> List:
+def get_single_moves(square: int) -> List:
   attacks = 0
 
-  tr, tf = square//4, square%4
-  print(tr, tf)
+  tr, tf = square // 8, square % 8
+  print(f"Square: {SQUARE_NAMES[square]}")
 
-  for r in range(tr+1, 8):
-    for f in range(tf+1, 4):
-      next_square_index = r*4 + f
-      print(next_square_index)
-      print(f"r: {r}, f: {f}")
-      print(f"Checking square: {SQUARE_NAMES[next_square_index]}")
-      attacks |= (1 << next_square_index)
-      if (1 << next_square_index) & BB_OCCUPIED:
-        break
+  # up-right
+  for r, f in zip(range(tr+1, 8), range(tf+1, 8)):
+    square = (r*8 + f)
+    print(f"Checking square: {SQUARE_NAMES[square]}")
+    if (1 << square) & BB_OCCUPIED:
+      print("Last check reached edge")
+      break
+    attacks |= (1 << square)
+  
+  # up-left
+  for r, f in zip(range(tr+1, 8), range(tf-1, -1, -1)):
+    square = (r*8 + f)
+    print(f"Checking square: {SQUARE_NAMES[square]}")
+
+    if (1 << square) & BB_OCCUPIED:
+      print("Last check reached edge")
+      break
+
+    attacks |= (1 << square)
 
   return attacks
 
 def print_board():
   builder = []
-  pieces_in_a_row = 0
+
   for square in SQUARES_180:
     piece = piece_at(square)
+
     if piece:
       builder.append(piece.symbol())
     else:
       builder.append(".")
-    
-    pieces_in_a_row += 1
 
-    if pieces_in_a_row == 4:
-      pieces_in_a_row = 0
-      builder.append("\n")
+    if BB_SQUARES[square] & BB_FILE_H:
+      if square != H1:
+        builder.append("\n")
     else:
-      builder.append(" ")
+        builder.append(" ")
 
   return "".join(builder)
 
@@ -191,7 +190,6 @@ def piece_type_at(square):
 
   elif BB_DOUBLES & mask:
     return DOUBLE
-
 
 print(print_board())
 moves = get_moves(D2)
