a2942d95331b901c9be4c29863c1931456e619f3
f0ti <foti.kerkeshi@hotmail.com>
2023-01-10 20:49:07 +0100

    sudooo

--- isg/docs/build.sh
+++ isg/docs/build.sh
@@ -1,16 +1,12 @@
-#!/usr/bin/sh
-
 # install mkdocs
 pip install mkdocs
 
 # install caddy as proxy
-apt install -y debian-keyring debian-archive-keyring apt-transport-https
+sudo apt apt install -y debian-keyring debian-archive-keyring apt-transport-https
 curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/gpg.key' | sudo gpg --dearmor -o /usr/share/keyrings/caddy-stable-archive-keyring.gpg
 curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/debian.deb.txt' | sudo tee /etc/apt/sources.list.d/caddy-stable.list
-apt update
-apt install caddy
+sudo apt update
+sudo apt install caddy
 
 # run app
 mkdocs build
-caddy file-server
-
